1644549f475cfad3d3aa17c22d1557f00f1dde3e
Aditya Lohar <adityalohar5838@gmail.com>
2025-08-13 14:18:10 +0530

    KickkOff sheet Complete 1.1

--- src/CompanyComponent/KickOff/KickOffPDF.jsx
+++ src/CompanyComponent/KickOff/KickOffPDF.jsx
@@ -59,6 +59,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderStyle: "solid",
     borderColor: "#eee",
+    //  textAlign: "center",
   },
 
   tableCellBlank: {
@@ -76,12 +77,41 @@ const styles = StyleSheet.create({
     borderTop: 1,
     borderStyle: "solid",
     borderColor: "#eee",
+    textAlign: "center",
+  },
+
+  tableCell2: {
+    flex: 1,
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 0,
+    borderTop: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+
   },
 
   highlighted: { backgroundColor: "#FFFFAA" },
   signatureCell: {
-    minHeight: 30,
+    minHeight: 40,
+    minWidth: 120, // Adjust width as needed
     justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#eee",
+    padding: 5,
+    marginRight: -1, // Avoid double border
+    marginBottom: -1,
+  },
+  footer: {
+    marginTop: 40,
+    alignItems: "flex-end",
+  },
+
+  signatureTable: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
     borderWidth: 1,
     borderColor: "#eee",
   },
@@ -98,6 +128,34 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     padding: 4,
   },
+  ShipAddRowcol: {
+    flex: 1,
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 0,
+    borderTop: 0,
+
+    borderColor: "#eee",
+  },
+  FsbRow: {
+    flex: 1,
+    padding: 4,
+
+    borderRightWidth: 1,
+    borderBottomWidth: 0,
+    borderTop: 1,
+    borderColor: "#eee",
+  },
+
+  FstRow: {
+    flex: 1,
+    padding: 4,
+
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderTop: 0,
+    borderColor: "#eee",
+  },
 
   prjTitle: {
     fontSize: 10,
@@ -105,6 +163,146 @@ const styles = StyleSheet.create({
     textAlign: "center",
     flex: 1,
     padding: 4,
+  },
+
+  tableTH: {
+    fontWeight: "bold",
+    textAlign: "center",
+    flex: 1,
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+  },
+
+  tableTH1: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 72.5,
+  },
+
+  tableTH2: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 50.7,
+  },
+
+  tableTH3: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 217.5,
+  },
+  tableTH4: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 152.9,
+  },
+
+  tableTH5: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 52,
+  },
+
+  tableTHM3: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 50.7,
+  },
+  tableTHM4: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 50.7,
+  },
+  tableTHM5: {
+    fontWeight: "bold",
+    textAlign: "center",
+
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+    width: 116.1,
+  },
+  tableTHMQDSMain: {
+    // flexDirection: "row",
+    width: 152.9,
+    borderColor: "#eee",
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+  },
+
+  tableTHMQDSMainRow2: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    marginTop: 17,
+  },
+  tableTHMQDSMainRow2Col: {
+    fontWeight: "bold",
+    textAlign: "center",
+    flex: 1,
+    padding: 4,
+    borderRightWidth: 1,
+    borderBottomWidth: 0,
+    borderTop: 1,
+    borderStyle: "solid",
+    borderColor: "#eee",
+  },
+
+  tableTHMQDSMaintitle: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  txteCenter: {
+    textAlign: "center",
+  },
+  txteBold: {
+    fontWeight: "bold",
   },
 });
 
@@ -146,8 +344,8 @@ const KickOffPDF = ({ data }) => {
             <View style={styles.Rowcol1}>
               <Text>{kickOffInfo.customerName}</Text>{" "}
             </View>
-            <View style={styles.Rowcol1}>
-              <Text> Customer Billing</Text>
+            <View style={[styles.Rowcol1, { flex: 2.0 }]}>
+              <Text>Customer Billing/ Shipping Address: </Text>
             </View>
           </View>
         </View>
@@ -160,7 +358,37 @@ const KickOffPDF = ({ data }) => {
             <View style={styles.Rowcol1}>
               <Text> {kickOffInfo.projectName}</Text>{" "}
             </View>
+            <View style={[styles.ShipAddRowcol, { flex: 2.0 }]}></View>
+          </View>
+        </View>
+
+        <View style={styles.section1}>
+          <View style={styles.MainRow1}>
             <View style={styles.Rowcol1}>
+              {" "}
+              <Text> Kick off Date:</Text>
+            </View>
+            <View style={styles.Rowcol1}>
+              <Text>{kickOffInfo.kickOffDate} </Text>{" "}
+            </View>
+            <View style={[styles.ShipAddRowcol, { flex: 2.0 }]}>
+              <Text>Billing Address: </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.section1}>
+          <View style={styles.MainRow1}>
+            <View style={styles.Rowcol1}>
+              {" "}
+              <Text> Delivery:</Text>
+            </View>
+            <View style={styles.Rowcol1}>
+              <Text>
+                T0: {kickOffInfo.startDate} ,T1 {kickOffInfo.endDate}
+              </Text>{" "}
+            </View>
+            <View style={[styles.ShipAddRowcol, { flex: 2.0 }]}>
               <Text>{kickOffInfo.billingAddress} </Text>
             </View>
           </View>
@@ -174,22 +402,8 @@ const KickOffPDF = ({ data }) => {
             <View style={styles.Rowcol1}>
               <Text>{kickOffInfo.contactPersonName}</Text>{" "}
             </View>
-            <View style={styles.Rowcol1}>
+            <View style={[styles.ShipAddRowcol, { flex: 2.0 }]}>
               <Text>Shipping Address: </Text>
-            </View>
-          </View>
-        </View>
-        <View style={styles.section1}>
-          <View style={styles.MainRow1}>
-            <View style={styles.Rowcol1}>
-              {" "}
-              <Text> Kick off Date:</Text>
-            </View>
-            <View style={styles.Rowcol1}>
-              <Text>{kickOffInfo.kickOffDate} </Text>{" "}
-            </View>
-            <View style={styles.Rowcol1}>
-              <Text> {kickOffInfo.shippingAddress}</Text>
             </View>
           </View>
         </View>
@@ -198,15 +412,13 @@ const KickOffPDF = ({ data }) => {
           <View style={styles.MainRow1}>
             <View style={styles.Rowcol1}>
               {" "}
-              <Text> Delivery:</Text>
+              <Text>Contact No:</Text>
             </View>
             <View style={styles.Rowcol1}>
-              <Text>
-                {kickOffInfo.startDate} to {kickOffInfo.endDate}
-              </Text>{" "}
+              <Text>{kickOffInfo.mobileNumber}</Text>{" "}
             </View>
-            <View style={styles.Rowcol1}>
-              <Text> </Text>
+            <View style={[styles.FstRow, { flex: 2.0 }]}>
+              <Text>{kickOffInfo.shippingAddress}</Text>
             </View>
           </View>
         </View>
@@ -225,17 +437,32 @@ const KickOffPDF = ({ data }) => {
           <View style={[styles.table, { fontSize: 9 }]}>
             {/* TABLE HEADER */}
             <View style={styles.tableRow}>
-              <Text style={[styles.tableCell, { flex: 1.5 }]}>
-                Part Name/Material/Thk
-              </Text>
-              <Text style={styles.tableCell}>Design</Text>
-              <Text style={styles.tableCell}>WO NO</Text>
-              <Text style={styles.tableCell}>OP NO</Text>
-              <Text style={styles.tableCell}>Process</Text>
-              <Text style={styles.tableCell}>L</Text>
-              <Text style={styles.tableCell}>W</Text>
-              <Text style={styles.tableCell}>H</Text>
-              <Text style={styles.tableCell}>Remarks</Text>
+              <Text style={[styles.tableTH1]}>Scope of Work:</Text>
+              <Text style={styles.tableTH2}>Design</Text>
+              <Text style={[styles.tableTH3]}>Manufacturing</Text>
+              <Text style={styles.tableTH4}>Checking Fixture</Text>
+              <Text style={styles.tableTH5}>PRESS TOOL</Text>
+            </View>
+
+            <View style={styles.tableRow}>
+              <Text style={[styles.tableTH1]}>Part Name/Material/Thk</Text>
+              <Text style={styles.tableTH2}>Part Image</Text>
+              <Text style={styles.tableTHM3}>WO NO</Text>
+              <Text style={styles.tableTHM4}>OP NO</Text>
+              <Text style={[styles.tableTHM5]}>Process</Text>
+              <View style={styles.tableTHMQDSMain}>
+                <Text style={styles.tableTHMQDSMaintitle}>
+                  Quoted Die Sizes (mm)
+                </Text>
+
+                <View style={styles.tableTHMQDSMainRow2}>
+                  <Text style={styles.tableTHMQDSMainRow2Col}>L</Text>
+                  <Text style={styles.tableTHMQDSMainRow2Col}>W</Text>
+                  <Text style={styles.tableTHMQDSMainRow2Col}>H</Text>
+                </View>
+              </View>
+
+              <Text style={styles.tableTH}>Remarks</Text>
             </View>
 
             {kickOffItemsList.map((item) => {
@@ -250,8 +477,14 @@ const KickOffPDF = ({ data }) => {
                     {/* Merge first two columns (only in the first row) */}
                     {idx === 0 ? (
                       <>
-                        <Text style={[styles.tableCell1, { flex: 1.5 }]}>
-                          {item.partName} / {item.material} / {item.thickness}
+                        <Text style={[styles.tableCell2, { flex: 1.5 }]}>
+                          <Text>
+                            {item.partName}
+                            {"\n"}
+                            Matl = {item.material}
+                            {"\n"}
+                            Thk = {item.thickness}
+                          </Text>
                         </Text>
                         <View style={styles.tableCell1}>
                           {item.imageList &&
@@ -283,10 +516,18 @@ const KickOffPDF = ({ data }) => {
                     <Text style={styles.tableCell}>
                       {proc.operationNumber || ""}
                     </Text>
-                    <Text style={styles.tableCell}>{proc.process}</Text>
-                    <Text style={styles.tableCell}>{proc.length}</Text>
-                    <Text style={styles.tableCell}>{proc.width}</Text>
-                    <Text style={styles.tableCell}>{proc.height}</Text>
+                    <Text style={[styles.tableCell, { flex: 2.5 }]}>
+                      {proc.process}
+                    </Text>
+                    <Text style={[styles.tableCell, styles.txteCenter]}>
+                      {proc.length}
+                    </Text>
+                    <Text style={[styles.tableCell, styles.txteCenter]}>
+                      {proc.width}
+                    </Text>
+                    <Text style={[styles.tableCell, styles.txteCenter]}>
+                      {proc.height}
+                    </Text>
                     <Text style={styles.tableCell}>{proc.remarks}</Text>
                   </View>
                 ));
@@ -323,7 +564,7 @@ const KickOffPDF = ({ data }) => {
 
         {/* Customer Requirements Table */}
         <View style={styles.section}>
-          <Text>Customer Requirements</Text>
+          <Text style={styles.txteBold}>Customer Requirements</Text>
           <View style={styles.table1}>
             {[
               "Inserts (Main/CAM)",
@@ -341,17 +582,21 @@ const KickOffPDF = ({ data }) => {
               "Remarks",
             ].map((type) => (
               <View style={styles.tableRow1} key={type}>
-                <Text style={[styles.tableCell, { flex: 1.5 }]}>{type}</Text>
-                <Text style={styles.tableCell}>
+                <Text
+                  style={[styles.tableCell, { flex: 1.5 }, styles.txteBold]}
+                >
+                  {type}
+                </Text>
+                <Text style={[styles.tableCell, styles.txteCenter]}>
                   {getReqValue(requirementList, type, "One")}
                 </Text>
-                <Text style={styles.tableCell}>
+                <Text style={[styles.tableCell, styles.txteCenter]}>
                   {getReqValue(requirementList, type, "Two")}
                 </Text>
-                <Text style={styles.tableCell}>
+                <Text style={[styles.tableCell, styles.txteCenter]}>
                   {getReqValue(requirementList, type, "Three")}
                 </Text>
-                <Text style={styles.tableCell}>
+                <Text style={[styles.tableCell, styles.txteCenter]}>
                   {getReqValue(requirementList, type, "Four")}
                 </Text>
               </View>
@@ -362,26 +607,22 @@ const KickOffPDF = ({ data }) => {
         {/* Signature Table */}
         <View style={styles.section}>
           <Text>Signature</Text>
-          <View
-            style={[
-              styles.table,
-              { flexDirection: "row", justifyContent: "space-between" },
-            ]}
-          >
+          <View style={styles.table1}>
             {listofSingnature.map((sig) => (
-              <View style={styles.signatureCell} key={sig.id}>
-                <Text>{sig.departments}</Text>
-                <Text>Sign</Text>
+              <View style={styles.tableRow1} key={sig.id}>
+                <Text style={styles.tableCell}>{sig.departments}</Text>
+                <Text style={styles.tableCell}>{sig.headName}</Text>
+                <Text style={[styles.tableCell, { flex: 0.5 }]}>Sign</Text>
+                <Text style={styles.tableCell}></Text>
               </View>
             ))}
           </View>
-        </View>
-        {/* Footer */}
-        <View
-          style={[styles.section, { marginTop: 30, alignItems: "flex-end" }]}
-        >
-          <Text>Mr. Shriram Handibag</Text>
-          <Text>PLANNETTO TOOLTECH PVT LTD</Text>
+
+          {/* Footer */}
+          <View style={styles.footer}>
+            <Text>Mr. Shriram Handibag</Text>
+            <Text>PLANNETTO TOOLTECH PVT LTD</Text>
+          </View>
         </View>
       </Page>
     </Document>
